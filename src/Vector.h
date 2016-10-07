563563f933c91facfb0c0c5e03a29112536af131
Arafo <rafa_marcen@hotmail.com>
2016-10-07 16:01:35 +0200

    Añadida clase para representar vectores (Sin acabar)

---
+++ src/Vector.h
@@ -0,0 +1,18 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+class Vector {
+public:
+
+	float x, y, z;
+
+	// Constructores
+	 Vector() : x(0f), y(0f), z(0f) {}
+	 Vector(float valor) : x(valor), y(valor), z(valor) {}
+	 Vector(float valor_x, float valor_y, float valor_z) : x(valor_x), y(valor_y), z(valor_z) {}
+
+	// Funciones
+	
+	};
+
+#endif
