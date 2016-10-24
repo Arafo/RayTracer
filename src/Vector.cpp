672220a1b58609d6475d6b3ba5135edfd2243337
Arafo <rafa_marcen@hotmail.com>
2016-10-24 17:39:54 +0200

    Añadido método para normalizar vectores

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,31 +1,46 @@
+#include <math.h>
 #include "Vector.h"
 
 // Producto escalar
 float Vector::escalar(Vector const &v) const {
-   return x * v.x + y * v.y + z * v.z;
+	return x * v.x + y * v.y + z * v.z;
 }
 
 // Producto vectorial
 Vector Vector::vectorial(Vector const &v) const {
-   return Vector(y * v.z - v.y * z, v.x * z - x * v.z, x * v.y - v.x * y);
+	return Vector(y * v.z - v.y * z, v.x * z - x * v.z, x * v.y - v.x * y);
+}
+
+// Normalizar
+Vector Vector::normalizar() {
+	return (*this) /= sqrtf(x * x + y * y + z * z);
 }
 
 // Suma de vectores
 Vector Vector::operator + (Vector const &v) const {
-   return Vector(x + v.x, y + v.y, z + v.z);
+	return Vector(x + v.x, y + v.y, z + v.z);
 }
 
 // Resta de vectores
 Vector Vector::operator - (Vector const &v) const {
-   return Vector(x - v.x, y - v.y, z - v.z);
+	return Vector(x - v.x, y - v.y, z - v.z);
 }
 
 // Multiplicación de vectores
 Vector Vector::operator * (Vector const &v) const {
-   return Vector(x * v.x, y * v.y, z * v.z);
+	return Vector(x * v.x, y * v.y, z * v.z);
 }
 
 // División de vectores
 Vector Vector::operator / (Vector const &v) const {
-   return Vector(x / v.x, y / v.y, z / v.z);
+	return Vector(x / v.x, y / v.y, z / v.z);
+}
+
+// División de vectores
+Vector & Vector::operator /= (Vector const &v) {
+	x /= v.x;
+	y /= v.y;
+	z /= v.z;
+
+	return * this;
 }
--- src/Vector.h
+++ src/Vector.h
@@ -7,17 +7,19 @@ public:
 	float x, y, z;
 
 	// Constructores
-	 Vector() : x(0.f), y(0.f), z(0.f) {}
-	 Vector(float valor) : x(valor), y(valor), z(valor) {}
-	 Vector(float valor_x, float valor_y, float valor_z) : x(valor_x), y(valor_y), z(valor_z) {}
+	Vector() : x(0.f), y(0.f), z(0.f) {}
+	Vector(float valor) : x(valor), y(valor), z(valor) {}
+	Vector(float valor_x, float valor_y, float valor_z) : x(valor_x), y(valor_y), z(valor_z) {}
 
 	// Funciones
-	 float escalar(Vector const &v) const;
-	 Vector vectorial(Vector const &v) const;
-	 Vector operator + (Vector const &v) const;
-	 Vector operator - (Vector const &v) const;
-	 Vector operator * (Vector const &v) const;
-	 Vector operator / (Vector const &v) const;
- 	};
+	float escalar(Vector const &v) const;
+	Vector vectorial(Vector const &v) const;
+	Vector normalizar();
+	Vector operator + (Vector const &v) const;
+	Vector operator - (Vector const &v) const;
+	Vector operator * (Vector const &v) const;
+	Vector operator / (Vector const &v) const;
+	Vector & operator /= (Vector const &v);
+};
 
 #endif
