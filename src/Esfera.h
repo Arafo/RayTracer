5d110dc6b652dcfa85d926975920f41ba5d5a629
Jaime <Chicos@Chicos-PC>
2016-10-24 18:34:31 +0200

    Clase para la creación de esferas y el tratamiento de las mismas.

---
+++ src/Esfera.h
@@ -0,0 +1,23 @@
+#ifndef __ESFERA_H__
+#define __ESFERA_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <Vector.h>
+
+class Esfera {
+public:
+	float radius;
+	Vector center;
+
+	//Constructor
+	Esfera() : radius(1), center(Vector()) {}
+	Esfera(int radius_, Vector center_) : radius(radius_), center(center_) {}
+
+	//Funciones
+	bool interseccion(Rayo ray);
+};
+
+#endif
