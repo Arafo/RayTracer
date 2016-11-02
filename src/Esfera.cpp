09d173ae073a5130b37bb3a7beb96ecc9a1ccd6e
Asdruval <lospuig.jaime@gmail.com>
2016-11-02 19:47:37 +0100

    Intersecciones
    Añadida funcion de calculo de intersecciones

--- src/Esfera.cpp
+++ src/Esfera.cpp
@@ -3,5 +3,11 @@
 // Devueve true si el rayo intersecta con esta esfera.
 // Devuelve false en caso contrario
 bool Esfera::interseccion(Rayo rayo) {
+	//Comprobacion geometrica. Si tF es mayor que
+	//el radio al cuadrado, podemos asegurar que no
+	Vector L = centro - rayo.origen;
+	float tD = L.escalar(rayo.direccion);
+	float tF = L.escalar(L) - tD*tD;
+	if (tF > (radio + radio)) return false;
 	return true;
 }
