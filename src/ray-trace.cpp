d226964c3a3cf31e0ec8da56e02a12982395ba1c
Arafo <rafa_marcen@hotmail.com>
2016-11-02 19:25:26 +0100

    Añadido todo lo necesario para pintar objetos sin iluminación

--- src/ray-trace.cpp
+++ src/ray-trace.cpp
@@ -1,32 +1,43 @@
 #include<iostream>
+#include <vector>
 
 #include "Vector.h"
 #include "Imagen.h"
+#include "Color.h"
+#include "Rayo.h"
+#include "Camara.h"
+#include "Objeto.h"
+#include "Esfera.h"
+#include "Luz.h"
 
 using namespace std;
 
+std::vector<Objeto*> objetos;
+std::vector<Luz*> luces;
+
+// Funciones
+void addObjeto(Objeto* objeto);
+void addLuz(Luz* luz);
+Color trazarRayo(const Rayo &rayo);
+
 int main()
 {
-	Vector v1(1);
-	Vector v2(1.5, 2.5, 3.5);
-
-	cout << "Productor escalar: " << v1.escalar(v2) << endl;
-	Vector v3 = v1.vectorial(v2);  
-	cout << "Productor vectorial: (" << v3.x << ", " << v3.y << ", " << v3.z << ") "<< endl;
-	v3 = v1 + v2;
-	cout << "Suma de vectores: (" << v3.x << ", " << v3.y << ", " << v3.z << ") "<< endl;
-	v3 = v1 - v2;
-	cout << "Resta de vectores: (" << v3.x << ", " << v3.y << ", " << v3.z << ") "<< endl;
-	v3 = v1 * v2;
-	cout << "Multiplicacion de vectores: (" << v3.x << ", " << v3.y << ", " << v3.z << ") "<< endl;
-	v3 = v1 / v2;
-	cout << "Division de vectores: (" << v3.x << ", " << v3.y << ", " << v3.z << ") "<< endl;
 
 	int w = 800;
 	int h = 600;
-	int columnas = 0;
 
 	Imagen imagen(w, h);
+	Camara camara(Vector(0, 0, 100), Vector(0.0, 0.0, 0.0), Vector(0.0, 1.0, 0.0), 384);
+
+	int columnas = 0;
+
+	// Escena
+	addObjeto(new Esfera(Vector(10, 0, 0), 30));
+	addObjeto(new Esfera(Vector(-7, -0.5, 90), 2));
+	addObjeto(new Esfera(Vector(-40, 10, -20), 30));
+	addObjeto(new Esfera(Vector(3400, 0, -10000), 2800));
+
+	addLuz(new Luz(Vector(50, 50, 1000), 0.9));
 
 	for (int x = 0; x < w; x++) {
 
@@ -37,7 +48,20 @@ int main()
 		fflush(stdout);
 
 		for (int y = 0; y < h; y++) {
-			Color color(x, y, x + y);
+
+			float rayoX = (x - w / 2) / 2.0f;
+			float rayoY = (y - h / 2) / 2.0f;
+			float anchuraPixel = rayoX - (x + 1 - w / 2) / 2.0f;
+			float inicioX = rayoX - anchuraPixel / 2.0f;
+			float inicioY = rayoY - anchuraPixel / 2.0f;
+			float escala = camara.f / (float)w;
+
+			Vector punto = camara.direccion - 
+				(camara.u * (inicioX + anchuraPixel) * escala) + 
+				(camara.v * (inicioY + anchuraPixel) * escala);
+			Rayo rayo(camara.posicion, punto - camara.posicion);
+			//cout << "Rayo: " << rayo.origen << " -> " << rayo.direccion << endl;
+			Color color = trazarRayo(rayo);
 			imagen.pintar(x, y, color);
 		}
 	}
@@ -45,4 +69,29 @@ int main()
 	imagen.savePPM("imagen.ppm");
 
 	return 0;
+}
+
+//
+Color trazarRayo(const Rayo &rayo) {
+	bool interseccion;
+	for (vector<Objeto*>::iterator itr = objetos.begin(); itr < objetos.end(); itr++) {
+		 interseccion = (*itr)->interseccion(rayo);
+		 if (interseccion) 
+		 	break;
+	}
+
+	if (interseccion) {
+		return Color(.4, .3, .7);
+	}
+	return Color();
+}
+
+// Añade un objeto a la lista de objetos
+void addObjeto(Objeto* objeto) {
+	objetos.push_back(objeto);
+}
+
+// Añade una luz a la lista de luces
+void addLuz(Luz* luz) {
+	luces.push_back(luz);
 }
