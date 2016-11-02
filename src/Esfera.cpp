fe374768015e181ab4cf2a40c89f39e2a67f657a
Arafo <rafa_marcen@hotmail.com>
2016-11-02 20:09:39 +0100

    Intersección rayo-esfera corregida

--- src/Esfera.cpp
+++ src/Esfera.cpp
@@ -3,11 +3,14 @@
 // Devueve true si el rayo intersecta con esta esfera.
 // Devuelve false en caso contrario
 bool Esfera::interseccion(Rayo rayo) {
-	//Comprobacion geometrica. Si tF es mayor que
-	//el radio al cuadrado, podemos asegurar que no
-	Vector L = centro - rayo.origen;
-	float tD = L.escalar(rayo.direccion);
-	float tF = L.escalar(L) - tD*tD;
-	if (tF > (radio + radio)) return false;
+	// Comprobacion geometrica. Si tF es mayor que
+	// el radio al cuadrado, podemos asegurar que no
+	Vector L = rayo.origen - centro;
+	float a = rayo.direccion.escalar(rayo.direccion);
+	float b = L.escalar(rayo.direccion) * 2;
+	float c = L.escalar(L) - (radio * radio);
+
+	float d = b * b - 4 * a * c;
+	if (d <= 0) return false;
 	return true;
 }
