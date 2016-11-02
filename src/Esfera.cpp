ef7fbe121738cc91775d4f714aa88c09824f8597
Asdruval <lospuig.jaime@gmail.com>
2016-11-02 19:55:52 +0100

    Tilde
    Cagoentojoderconlastildes

--- src/Esfera.cpp
+++ src/Esfera.cpp
@@ -5,6 +5,7 @@
 bool Esfera::interseccion(Rayo rayo) {
 	//Comprobacion geometrica. Si tF es mayor que
 	//el radio al cuadrado, podemos asegurar que no
+	//habra intersecciones.
 	Vector L = centro - rayo.origen;
 	float tD = L.escalar(rayo.direccion);
 	float tF = L.escalar(L) - tD*tD;
