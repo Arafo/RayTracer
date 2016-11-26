5b7bb468b38757bdb1b4ab951d38be4eb1eee5a0
Arafo <rafa_marcen@hotmail.com>
2016-11-26 12:11:38 +0100

    Añadida refracción

--- src/Rayo.h
+++ src/Rayo.h
@@ -8,10 +8,24 @@ class Rayo {
 public:
 	Vector origen;
 	Vector direccion;
+	int reflexiones;
+	float iRefracOrigen;
 
 	// Constructores
-	Rayo() : origen(Vector()), direccion(Vector()) {}
-	Rayo(Vector origen_, Vector direccion_) : origen(origen_), direccion(direccion_) {}
+	Rayo() : origen(Vector()), direccion(Vector()), reflexiones(-1), iRefracOrigen(1.0) {}
+
+	Rayo(Vector origen_, Vector direccion_, int reflexiones_) : origen(origen_), direccion(direccion_), reflexiones(reflexiones_) {
+		direccion = direccion_.normalizar();
+		origen += (direccion / 1);
+	}
+
+	Rayo(Vector origen_, Vector direccion_, int reflexiones_, float iRefracOrigen_) : 
+	origen(origen_), direccion(direccion_), reflexiones(reflexiones_), iRefracOrigen(iRefracOrigen_) {
+		direccion = direccion_.normalizar();
+		origen += (direccion / 1);
+	}
+
+
 
 };
 
--- src/ray-trace.cpp
+++ src/ray-trace.cpp
@@ -23,8 +23,11 @@ Color trazarRayo(const Rayo &rayo);
 Color calcularIluminacion(const Interseccion& interseccion);
 Color calcularLuzAmbiental(const Interseccion& interseccion, const Color& color);
 Color calcularLuzDifusaEspecular(const Interseccion& interseccion, const Color& color);
-Color calcularLuzEspecular(const Interseccion& intersection, Luz* light);
-bool estaEnSombra(const Rayo& rayo);
+Color calcularLuzEspecular(const Interseccion& interseccion, Luz* luz);
+Color calcularLuzReflexionRefraccion(const Interseccion& interseccion);
+Vector refractar(const Vector& normal, const Vector& direccion, float n);
+float reflectancia(const Vector& normal, const Vector& direccion, float n1, float n2);
+bool estaEnSombra(const Rayo& rayo, float distanciaLuz);
 Interseccion interseccionMasCercana(const Rayo& rayo);
 
 int main() {
@@ -37,15 +40,32 @@ int main() {
 	int columnas = 0;
 
 	// Escena
-	addObjeto(new Esfera(Vector(10, 0, 0), 30));
-	addObjeto(new Esfera(Vector(-44, 0, -20), 20));
-	//addObjeto(new Esfera(Vector(-40, 4, 0), 10));
-	//addObjeto(new Esfera(Vector(-40, 10, -20), 30));
-	//addObjeto(new Esfera(Vector(3400, 0, -10000), 2800));
-	//addObjeto(new Esfera(Vector(-7, -0.5, 90), 2));
-	addObjeto(new Esfera(Vector(w/4, -1000030, 1000), 1000000));
+	addObjeto(new Esfera(Vector(10, 0, 0), 30, Color(0, 0, 0), 50, -1, 1.05, 0));
+	addObjeto(new Esfera(Vector(10, 80, -200), 30, Color(0.219, 0.352, 0.949), -1, -1, -1, 0));
+	addObjeto(new Esfera(Vector(-44, 0, -20), 20, Color(0.7, 0.7, 0.0), 100, 0.4, -1, 0));
+	addObjeto(new Esfera(Vector(w/4, -1000030, 1000), 1000000, Color(0.211, 0.694, 0.749 ), -1, -1, -1, 0));
 
 	addLuz(new Luz(Vector(50, 50, 100), 0.9));
+
+	// Cornel Box
+	/*addObjeto(new Esfera(Vector(10, 0, 60), 12, Color(0.949, 0.219, 0.352), 16, -1, 0, 0));
+	addObjeto(new Esfera(Vector(-24, 0, 60), 10, Color(0.7, 0.7, 0.0), 100, 0.4, 0, 0));
+
+	addObjeto(new Esfera(Vector(w/4, -1000030, 1000), 1000000, Color(1, 1, 1), -1, 0, 0, 0));	// Suelo
+	addObjeto(new Esfera(Vector(-1000050, 0, 1000), 1000000, Color(0.8, 0, 0), -1, 0, 0, 0));	// Pared Izquierda
+	addObjeto(new Esfera(Vector(1000050, 0, 1000), 1000000, Color(0, 0.8, 0), -1, 0, 0, 0));	// Pared Derecha
+	addObjeto(new Esfera(Vector(w/4, 0, -999950), 1000000, Color(1, 1, 1), -1, 0, 0, 0));		// Pared Fondo
+	addObjeto(new Esfera(Vector(w/4, 1000040, 1000), 1000000, Color(1, 1, 1), -1, 0, 0, 0));	// Techo
+
+	addLuz(new Luz(Vector(10, 8, 90), 0.9));*/
+
+	// Otra escena
+	/*addObjeto(new Esfera(Vector(-200, -25, 0), 100, Color(1, 0, 0), 100, 0.75, 0, 0));
+	addObjeto(new Esfera(Vector(200, -25, 0), 100, Color(0, 1, 0), 100, 0.75, 0, 0));
+	addObjeto(new Esfera(Vector(0, 325, 0), 100, Color(0, 0, 1 ), 100, 0.75, 0, 0));
+
+	addLuz(new Luz(Vector(0, 100, 200), 0.5));*/
+
 
 	for (int x = 0; x < w; x++) {
 
@@ -67,7 +87,7 @@ int main() {
 			Vector punto = camara.direccion - 
 				(camara.u * inicioX * escala) + 
 				(camara.v * inicioY * escala);
-			Rayo rayo(camara.posicion, punto - camara.posicion);
+			Rayo rayo(camara.posicion, punto - camara.posicion, 1, 1.0);
 			//cout << punto << endl;
 			//cout << "Rayo: " << rayo.origen << " -> " << rayo.direccion << endl;
 			Color color = trazarRayo(rayo);
@@ -92,11 +112,12 @@ Color trazarRayo(const Rayo &rayo) {
 
 //
 Color calcularIluminacion(const Interseccion &interseccion) {
-	Color color = Color(.4, .3, .7); // De prueba
+	Color color = interseccion.objeto->color;// Color(.4, .3, .7); // De prueba
 	Color luzAmbiental = calcularLuzAmbiental(interseccion, color);
 	Color luzDifusaEspecular = calcularLuzDifusaEspecular(interseccion, color);
+	Color luzReflexionRefraccion = calcularLuzReflexionRefraccion(interseccion);
 
-	return luzAmbiental + luzDifusaEspecular;
+	return luzAmbiental + luzDifusaEspecular + luzReflexionRefraccion;
 }
 
 //
@@ -114,14 +135,15 @@ Color calcularLuzDifusaEspecular(const Interseccion &interseccion, const Color& 
 		Luz* luz = *itr;
 
 		Vector interseccionLuz = luz->posicion - interseccion.interseccion;
+		float distanciaLuz = interseccionLuz.longitud();
 		interseccionLuz.normalizar();
 		float producto = interseccion.normal.escalar(interseccionLuz);
 
 		if (producto >= 0.0f) {
 			// Rayo de sombra desde el punto de interseccion a la luz
-			Rayo rayoSombra = Rayo(interseccion.interseccion + 0.1, interseccionLuz);
+			Rayo rayoSombra = Rayo(interseccion.interseccion, interseccionLuz, 1, interseccion.rayo.iRefracOrigen);
 
-			if (estaEnSombra(rayoSombra)) {
+			if (estaEnSombra(rayoSombra, distanciaLuz)) {
 				// Sombra
 				continue;
 			}
@@ -136,7 +158,11 @@ Color calcularLuzDifusaEspecular(const Interseccion &interseccion, const Color& 
 //
 Color calcularLuzEspecular(const Interseccion& interseccion, Luz* luz) {
 	Color colorEspecular(0.0, 0.0, 0.0);
-	float brillo = 8;
+	float brillo = interseccion.objeto->iEspecular;
+
+	if (brillo < 0) {
+		return colorEspecular;
+	}
 
 	Vector espectador = (interseccion.rayo.origen - interseccion.interseccion).normalizar();
 	Vector lightOffset = (luz->posicion - interseccion.interseccion).normalizar();
@@ -156,9 +182,65 @@ Color calcularLuzEspecular(const Interseccion& interseccion, Luz* luz) {
 }
 
 //
-bool estaEnSombra(const Rayo& rayo) {
+Color calcularLuzReflexionRefraccion(const Interseccion& interseccion) {
+	Color colorReflexion;
+	Color colorRefraccion;
+
+	float indiceRefraccionOrigen = interseccion.rayo.iRefracOrigen;
+	float indiceRefraccionDestino = interseccion.objeto->iRefraccion;
+
+	float cantidadRefraccion = 0;
+
+	if (indiceRefraccionDestino > 0) {
+		cantidadRefraccion = 1 - reflectancia(interseccion.normal, interseccion.rayo.direccion, indiceRefraccionOrigen, indiceRefraccionDestino);
+	}
+	else {
+		return Color();
+	}
+
+	if (cantidadRefraccion > 0) {
+		Vector refractado = refractar(interseccion.normal, interseccion.rayo.direccion, indiceRefraccionOrigen / indiceRefraccionDestino);
+		Rayo rayoRefractado = Rayo(interseccion.interseccion, refractado, 1, indiceRefraccionDestino);
+		colorRefraccion = trazarRayo(rayoRefractado) * cantidadRefraccion;
+	}
+     
+	return colorReflexion + colorRefraccion;
+}
+
+//
+Vector refractar(const Vector& normal, const Vector& direccion, float n) {
+	float cosI = -normal.escalar(direccion);
+	float sinT2 = n * n * (1.0 - cosI * cosI);
+
+	if (sinT2 > 1.0) {
+		exit(EXIT_FAILURE);
+	}
+
+	float cosT = sqrt(1.0 - sinT2);
+	return direccion * n + normal * (n * cosI - cosT);
+}
+
+//
+float reflectancia(const Vector& normal, const Vector& direccion, float n1, float n2) {
+	float n = n1 / n2;
+	float cosI = -normal.escalar(direccion);
+	float sinT2 = n * n * (1.0 - cosI * cosI);
+
+	if (sinT2 > 1.0) {
+	// Total Internal Reflection.
+		return 1.0;
+	}
+
+	float cosT = sqrt(1.0 - sinT2);
+	float r0rth = (n1 * cosI - n2 * cosT) / (n1 * cosI + n2 * cosT);
+	float rPar = (n2 * cosI - n1 * cosT) / (n2 * cosI + n1 * cosT);
+	return (r0rth * r0rth + rPar * rPar) / 2.0;
+}
+
+//
+bool estaEnSombra(const Rayo& rayo, float distanciaLuz) {
 	Interseccion interseccionMin = interseccionMasCercana(rayo);
-	return interseccionMin.hayInterseccion;
+	return interseccionMin.hayInterseccion && interseccionMin.distancia < distanciaLuz;
 }
 
 //
