2fa91ae376368a755a7beae24947d07bfb12dd64
Arafo <rafa_marcen@hotmail.com>
2016-12-04 22:32:44 +0100

    Reflexión acabada

--- src/ray-trace.cpp
+++ src/ray-trace.cpp
@@ -25,7 +25,8 @@ Color calcularLuzAmbiental(const Interseccion& interseccion, const Color& color)
 Color calcularLuzDifusaEspecular(const Interseccion& interseccion, const Color& color);
 Color calcularLuzEspecular(const Interseccion& interseccion, Luz* luz);
 Color calcularLuzReflexionRefraccion(const Interseccion& interseccion);
-Vector refractar(const Vector& normal, const Vector& direccion, float n);
+Vector reflexion(const Vector& normal, const Vector& direccion);
+Vector refraccion(const Vector& normal, const Vector& direccion, float n);
 float reflectancia(const Vector& normal, const Vector& direccion, float n1, float n2);
 bool estaEnSombra(const Rayo& rayo, float distanciaLuz);
 Interseccion interseccionMasCercana(const Rayo& rayo);
@@ -35,29 +36,30 @@ int main() {
 	int h = 600;
 
 	Imagen imagen(w, h);
-	Camara camara(Vector(0, 0, 100), Vector(0.0, 0.0, 0.0), Vector(0.0, 1.0, 0.0), 384);
+	Camara camara(Vector(0, 0, 100), Vector(0.0, 0, 0.0), Vector(0.0, 1.0, 0.0), 60);
 
 	int columnas = 0;
 
 	// Escena
-	addObjeto(new Esfera(Vector(10, 0, 0), 30, Color(0, 0, 0), 50, -1, 1.05, 0));
+	/*addObjeto(new Esfera(Vector(10, 0, 0), 30, Color(0.721, 0.167, 0.323), 50, -1, -1, 0));
 	addObjeto(new Esfera(Vector(10, 80, -200), 30, Color(0.219, 0.352, 0.949), -1, -1, -1, 0));
-	addObjeto(new Esfera(Vector(-44, 0, -20), 20, Color(0.7, 0.7, 0.0), 100, 0.4, -1, 0));
-	addObjeto(new Esfera(Vector(w/4, -1000030, 1000), 1000000, Color(0.211, 0.694, 0.749 ), -1, -1, -1, 0));
+	addObjeto(new Esfera(Vector(-44, -10, -20), 20, Color(0.7, 0.7, 0.0), 100, 0.4, -1, 0));
+	//addObjeto(new Esfera(Vector(w/4, -1000030, 1000), 1000000, Color(0.211, 0.694, 0.749 ), -1, -1, -1, 0));
+	addObjeto(new Esfera(Vector(50, -100030, 0), 100000, Color(0.211, 0.694, 0.749 ), -1, 0.6, -1, 0));
 
-	addLuz(new Luz(Vector(50, 50, 100), 0.9));
+	addLuz(new Luz(Vector(50, 50, 100), 0.9));*/
 
 	// Cornel Box
-	/*addObjeto(new Esfera(Vector(10, 0, 60), 12, Color(0.949, 0.219, 0.352), 16, -1, 0, 0));
-	addObjeto(new Esfera(Vector(-24, 0, 60), 10, Color(0.7, 0.7, 0.0), 100, 0.4, 0, 0));
+	addObjeto(new Esfera(Vector(16, -26, -300), 12, Color(1, 1, 1), 16, -1, 0, 0));
+	addObjeto(new Esfera(Vector(-20, -30, -400), 10, Color(0.7, 0.7, 0.0), 100, 0.4, 0, 0));
 
-	addObjeto(new Esfera(Vector(w/4, -1000030, 1000), 1000000, Color(1, 1, 1), -1, 0, 0, 0));	// Suelo
-	addObjeto(new Esfera(Vector(-1000050, 0, 1000), 1000000, Color(0.8, 0, 0), -1, 0, 0, 0));	// Pared Izquierda
-	addObjeto(new Esfera(Vector(1000050, 0, 1000), 1000000, Color(0, 0.8, 0), -1, 0, 0, 0));	// Pared Derecha
-	addObjeto(new Esfera(Vector(w/4, 0, -999950), 1000000, Color(1, 1, 1), -1, 0, 0, 0));		// Pared Fondo
-	addObjeto(new Esfera(Vector(w/4, 1000040, 1000), 1000000, Color(1, 1, 1), -1, 0, 0, 0));	// Techo
+	addObjeto(new Esfera(Vector(w/4, -100030, 1000), 100000, Color(0.9, 0.9, 0.9), -1, -1, 0, 0));	// Suelo
+	addObjeto(new Esfera(Vector(-100042, 0, 1000), 100000, Color(0.8, 0, 0), -1, 0, 0, 0));	// Pared Izquierda
+	addObjeto(new Esfera(Vector(100042, 0, 1000), 100000, Color(0, 0.8, 0), -1, 0, 0, 0));	// Pared Derecha
+	addObjeto(new Esfera(Vector(w/4, 0, -100440), 100000, Color(0.9, 0.9, 0.9), -1, -1, 0, 0));		// Pared Fondo
+	addObjeto(new Esfera(Vector(w/4, 100030, 1000), 100000, Color(0.9, 0.9, 0.9), -1, 0, 0, 0));	// Techo
 
-	addLuz(new Luz(Vector(10, 8, 90), 0.9));*/
+	addLuz(new Luz(Vector(0, 20, -350), 0.9));
 
 	// Otra escena
 	/*addObjeto(new Esfera(Vector(-200, -25, 0), 100, Color(1, 0, 0), 100, 0.75, 0, 0));
@@ -65,7 +67,6 @@ int main() {
 	addObjeto(new Esfera(Vector(0, 325, 0), 100, Color(0, 0, 1 ), 100, 0.75, 0, 0));
 
 	addLuz(new Luz(Vector(0, 100, 200), 0.5));*/
-
 
 	for (int x = 0; x < w; x++) {
 
@@ -87,7 +88,7 @@ int main() {
 			Vector punto = camara.direccion - 
 				(camara.u * inicioX * escala) + 
 				(camara.v * inicioY * escala);
-			Rayo rayo(camara.posicion, punto - camara.posicion, 1, 1.0);
+			Rayo rayo(camara.posicion, punto - camara.posicion, 4, 1.0);
 			//cout << punto << endl;
 			//cout << "Rayo: " << rayo.origen << " -> " << rayo.direccion << endl;
 			Color color = trazarRayo(rayo);
@@ -186,20 +187,37 @@ Color calcularLuzReflexionRefraccion(const Interseccion& interseccion) {
 	Color colorReflexion;
 	Color colorRefraccion;
 
+	int reflexionesRestantes = interseccion.rayo.reflexiones;
+	//cout << reflexionesRestantes << endl;
+	
+	float indiceReflexion = interseccion.objeto->iReflexion;
 	float indiceRefraccionOrigen = interseccion.rayo.iRefracOrigen;
 	float indiceRefraccionDestino = interseccion.objeto->iRefraccion;
 
+	float cantidadReflexion = indiceReflexion;
 	float cantidadRefraccion = 0;
 
-	if (indiceRefraccionDestino > 0) {
-		cantidadRefraccion = 1 - reflectancia(interseccion.normal, interseccion.rayo.direccion, indiceRefraccionOrigen, indiceRefraccionDestino);
-	}
-	else {
+	if (reflexionesRestantes <= 0 || indiceReflexion <= 0 && indiceRefraccionDestino <= 0) {
 		return Color();
 	}
 
+	if (indiceRefraccionDestino > 0) {
+		//cantidadReflexion = reflectancia(interseccion.normal, interseccion.rayo.direccion, indiceRefraccionOrigen, indiceRefraccionDestino);
+		cantidadRefraccion = 1 - reflectancia(interseccion.normal, interseccion.rayo.direccion, indiceRefraccionOrigen, indiceRefraccionDestino);;
+	}
+
+	if (cantidadReflexion <= 0 && cantidadRefraccion <= 0) {
+		return Color();
+	}
+
+	if (cantidadReflexion > 0) {
+		Vector reflejado = reflexion(interseccion.normal, interseccion.rayo.direccion);
+		Rayo rayoReflejado = Rayo(interseccion.interseccion, reflejado, reflexionesRestantes - 1, indiceRefraccionDestino);
+		colorReflexion = trazarRayo(rayoReflejado) * cantidadReflexion;
+	}
+
 	if (cantidadRefraccion > 0) {
-		Vector refractado = refractar(interseccion.normal, interseccion.rayo.direccion, indiceRefraccionOrigen / indiceRefraccionDestino);
+		Vector refractado = refraccion(interseccion.normal, interseccion.rayo.direccion, indiceRefraccionOrigen / indiceRefraccionDestino);
 		Rayo rayoRefractado = Rayo(interseccion.interseccion, refractado, 1, indiceRefraccionDestino);
 		colorRefraccion = trazarRayo(rayoRefractado) * cantidadRefraccion;
 	}
@@ -214,7 +232,7 @@ Vector reflexion(const Vector& normal, const Vector& direccion) {
 }
 
 // Vector refractado
-Vector refractar(const Vector& normal, const Vector& direccion, float n) {
+Vector refraccion(const Vector& normal, const Vector& direccion, float n) {
 	float cosI = -normal.escalar(direccion);
 	float sinT2 = n * n * (1.0 - cosI * cosI);
 
