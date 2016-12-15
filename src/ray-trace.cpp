f7c60b268159a175b0dbe3f24df517b86d3ccbc3
Arafo <rafa_marcen@hotmail.com>
2016-12-15 18:11:02 +0100

    Añadido supersampling y pruebas con texturas

--- src/ray-trace.cpp
+++ src/ray-trace.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <vector>
+#include <ctime>
 
 #include "Vector.h"
 #include "Imagen.h"
@@ -10,8 +11,14 @@
 #include "Esfera.h"
 #include "Luz.h"
 #include "Interseccion.h"
+#include "Textura.h";
 
 using namespace std;
+
+bool LUZ_INDIRECTA = false;
+int LUZ_INDIRECTA_SAMPLES = 32;
+int MAX_REBOTES = 1;
+int ANTIALIASING = 1;
 
 std::vector<Objeto*> objetos;
 std::vector<Luz*> luces;
@@ -26,6 +33,7 @@ Color calcularLuzDifusaEspecular(const Interseccion& interseccion, const Color& 
 Color calcularLuzEspecular(const Interseccion& interseccion, Luz* luz);
 Color calcularLuzReflexionRefraccion(const Interseccion& interseccion);
 Color calcularLuzIndirecta(const Interseccion& interseccion, const Color& color);
+Vector muestreoSemiesfera(const Vector &n, const float &r1, const float &r2);
 Vector reflexion(const Vector& normal, const Vector& direccion);
 Vector refraccion(const Vector& normal, const Vector& direccion, float n);
 float reflectancia(const Vector& normal, const Vector& direccion, float n1, float n2);
@@ -33,13 +41,21 @@ bool estaEnSombra(const Rayo& rayo, float distanciaLuz);
 Interseccion interseccionMasCercana(const Rayo& rayo);
 
 int main() {
+
+	clock_t t1, t2;
+	t1 = clock();
+
 	int w = 800;
 	int h = 600;
 
 	Imagen imagen(w, h);
-	Camara camara(Vector(0, 0, 100), Vector(0.0, 0, 0.0), Vector(0.0, 1.0, 0.0), 60);
+	//Camara camara(Vector(0, 20, 100), Vector(0.0, 14, 0.0), Vector(0.0, 1.0, 0.0), 60);
 
 	int columnas = 0;
+
+	//imagen.readBMP("imagen.bmp");
+	//imagen.saveBMP("r1.bmp");
+	//exit(0);
 
 	// Escena
 	/*addObjeto(new Esfera(Vector(10, 0, 0), 30, Color(0.721, 0.167, 0.323), 50, -1, -1, 0));
@@ -50,18 +66,19 @@ int main() {
 
 	addLuz(new Luz(Vector(50, 50, 100), 0.9));*/
 
+	Textura tex1("../texturas/mapa_hd.ppm");
+
 	// Cornel Box
-	addObjeto(new Esfera(Vector(16, -26, -300), 12, Color(1, 1, 1), 16, -1, 0, 0));
-	addObjeto(new Esfera(Vector(-20, -30, -400), 10, Color(0.7, 0.7, 0), -1, 0.4, 0, 0));
+	/*addObjeto(new Esfera(Vector(16, -22, -300), 16, Color(0, 0, 0), -1, -1, 1.6, 0));
+	addObjeto(new Esfera(Vector(-26, -26, -380), 12, Color(0.7, 0.7, 0), 16, -1, -1, 0));
 
 	addObjeto(new Esfera(Vector(w/4, -100030, 1000), 100000, Color(0.9, 0.9, 0.9), -1, -1, 0, 0));	// Suelo
-	addObjeto(new Esfera(Vector(-100042, 0, 1000), 100000, Color(0.8, 0, 0), -1, -1, 0, 0));	// Pared Izquierda
-	addObjeto(new Esfera(Vector(100042, 0, 1000), 100000, Color(0, 0.8, 0), -1, -1, 0, 0));	// Pared Derecha
+	addObjeto(new Esfera(Vector(-100038, 0, 1000), 100000, Color(1, 0.32, 0.32), -1, -1, 0, 0));	// Pared Izquierda
+	addObjeto(new Esfera(Vector(100038, 0, 1000), 100000, Color(0.55, 0.76, 0.29), -1, -1, 0, 0));	// Pared Derecha
 	addObjeto(new Esfera(Vector(w/4, 0, -100440), 100000, Color(0.9, 0.9, 0.9), -1, -1, 0, 0));		// Pared Fondo
 	addObjeto(new Esfera(Vector(w/4, 100030, 1000), 100000, Color(0.9, 0.9, 0.9), -1, -1, 0, 0));	// Techo
-		
-	addLuz(new Luz(Vector(0, 20, -350), 0.9));
-	//addLuz(new Luz(Vector(0, -30, -350), 0.9));
+
+	addLuz(new Luz(Vector(0, 20, -350), 0.9));*/
 
 	// Otra escena
 	/*addObjeto(new Esfera(Vector(-200, -25, 0), 100, Color(1, 0, 0), 100, 0.75, 0, 0));
@@ -70,6 +87,14 @@ int main() {
 
 	addLuz(new Luz(Vector(0, 100, 200), 0.5));*/
 
+	// Escena textura
+	Camara camara(Vector(100, 0, 0), Vector(80, 0, -80), Vector(0, 1, 0), 60);
+	addObjeto(new Esfera(Vector(0, 0, -380), 30, Color(/*0.25, 0.77, 1*/0.7, 0.7, 0), -1, -1, -1, 0, tex1));
+	addObjeto(new Esfera(Vector(w/4, -100030, 1000), 100000, Color(0.25, 0.77, 1), -1, -1, 0, 0));	// Suelo
+	addLuz(new Luz(Vector(0, 40, -200), 0.9));
+	addLuz(new Luz(Vector(0, 40, -400), 0.9));
+
+	#pragma omp parallel for
 	for (int x = 0; x < w; x++) {
 
 		// Actualiza el porcentaje completado
@@ -83,17 +108,31 @@ int main() {
 			float rayoX = (x - w / 2) / 2.0f;
 			float rayoY = (y - h / 2) / 2.0f;
 			float anchuraPixel = rayoX - (x + 1 - w / 2) / 2.0f;
+			float anchuraSample = anchuraPixel / ANTIALIASING;
 			float inicioX = rayoX - anchuraPixel / 2.0f;
 			float inicioY = rayoY - anchuraPixel / 2.0f;
 			float escala = camara.f / (float)w;
+			float sample = 1.0 / (ANTIALIASING * ANTIALIASING);
 
-			Vector punto = camara.direccion - 
-				(camara.u * inicioX * escala) + 
-				(camara.v * inicioY * escala);
-			Rayo rayo(camara.posicion, punto - camara.posicion, 4, 1.0, 0);
 			//cout << punto << endl;
 			//cout << "Rayo: " << rayo.origen << " -> " << rayo.direccion << endl;
-			Color color = trazarRayo(rayo);
+			//int samples = 128;
+			Color color(0.0, 0.0, 0.0);
+			for (int xaa = 0; xaa < ANTIALIASING; xaa++) {
+				for (int yaa = 0; yaa < ANTIALIASING; yaa++) {
+					Vector punto = camara.direccion - 
+						(camara.u * (inicioX + (xaa * anchuraSample)) * escala) + 
+						(camara.v * (inicioY + (yaa * anchuraSample)) * escala);
+					Rayo rayo(camara.posicion, punto - camara.posicion, 4, 1.0, 0);
+			//for (int i = 0; i < samples; i++) {
+					color = color + (trazarRayo(rayo) * sample);
+			//}
+				}
+			}
+
+			//color.r = color.r / (samples);
+			//color.g = color.g / (samples);
+			//color.b = color.b / (samples);
 
 			imagen.pintar(x, y, color);
 		}
@@ -101,12 +140,16 @@ int main() {
 	imagen.saveBMP("imagen.bmp");
 	//imagen.savePPM("imagen.ppm");
 
+	t2 = clock();
+	float tiempo = ((float)t2 - (float)t1) / 1000;
+	cout << endl << "Tiempo: " << tiempo << " segundos" << endl;
+
 	return 0;
 }
 
 //
 Color trazarRayo(const Rayo &rayo) {
-	if (rayo.rebotes > 1) {
+	if (rayo.rebotes > MAX_REBOTES) {
 		return Color();
 	}
 
@@ -120,15 +163,38 @@ Color trazarRayo(const Rayo &rayo) {
 
 //
 Color calcularIluminacion(const Interseccion &interseccion) {
-	Color color = interseccion.objeto->color;
+	Color color = interseccion.objeto->getColor(interseccion.interseccion);
 	Color luzAmbiental = calcularLuzAmbiental(interseccion, color);
 	Color luzDifusaEspecular = calcularLuzDifusaEspecular(interseccion, color);
 	Color luzReflexionRefraccion = calcularLuzReflexionRefraccion(interseccion);
 
 	Color luzDirecta = luzAmbiental + luzDifusaEspecular + luzReflexionRefraccion;
+	if (!LUZ_INDIRECTA)
+		return luzDirecta;
+
 	Color luzIndirecta = calcularLuzIndirecta(interseccion, color);
 
-	return ((luzAmbiental + luzDifusaEspecular + luzReflexionRefraccion) / M_PI  + luzIndirecta * 2) * color;
+	//if (luzDirecta.r == 0.f && luzDirecta.g == 0.f && luzDirecta.b == 0.f) return luzIndirecta;
+	//return (luzDirecta + luzIndirecta) / 2.f;
+	return luzReflexionRefraccion + ((luzAmbiental + luzDifusaEspecular) / M_PI  + luzIndirecta * 2) * color;
+
+	//return ((luzAmbiental + luzDifusaEspecular + luzReflexionRefraccion) / M_PI + luzIndirecta * 2);
+}
+
+Vector random_unit_vector() {    
+	float x, y, z;
+	float r1 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	float r2 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+    float phi = 2.0 * M_PI * r2;
+    float sinTheta = sqrtf(1.0 - r1 * r1);
+
+    x = cosf(phi) * sinTheta;
+    y = sinf(phi) * sinTheta;
+    z = r1;
+
+    Vector direccion = Vector(x, y, z);
+    return direccion.normalizar();
 }
 
 //
@@ -145,22 +211,59 @@ Color calcularLuzDifusaEspecular(const Interseccion &interseccion, const Color& 
 	for (vector<Luz*>::iterator itr = luces.begin(); itr < luces.end(); itr++) {
 		Luz* luz = *itr;
 
-		Vector interseccionLuz = luz->posicion - interseccion.interseccion;
-		float distanciaLuz = interseccionLuz.longitud();
-		interseccionLuz.normalizar();
-		float producto = interseccion.normal.escalar(interseccionLuz);
+		if (luz->getTipo() == LUZ) {
+			Vector interseccionLuz = luz->posicion - interseccion.interseccion;
+			float distanciaLuz = interseccionLuz.longitud();
+			interseccionLuz.normalizar();
+			float producto = interseccion.normal.escalar(interseccionLuz);
 
-		if (producto >= 0.0f) {
-			// Rayo de sombra desde el punto de interseccion a la luz
-			Rayo rayoSombra = Rayo(interseccion.interseccion, interseccionLuz, 1, interseccion.rayo.iRefracOrigen, interseccion.rayo.rebotes);
+			if (producto >= 0.0f) {
+				// Rayo de sombra desde el punto de interseccion a la luz
+				Rayo rayoSombra = Rayo(interseccion.interseccion, interseccionLuz, 1, interseccion.rayo.iRefracOrigen, interseccion.rayo.rebotes);
 
-			if (estaEnSombra(rayoSombra, distanciaLuz)) {
-				// Sombra
-				continue;
+				if (estaEnSombra(rayoSombra, distanciaLuz)) {
+					// Sombra
+					continue;
+				}
+				colorDifuso = (colorDifuso + (color * producto)) * luz->intensidad;
+				colorEspecular = colorEspecular + calcularLuzEspecular(interseccion, luz);
 			}
-			colorDifuso = (colorDifuso + (color * producto)) * luz->intensidad;
-			colorEspecular = colorEspecular + calcularLuzEspecular(interseccion, luz);
 		}
+		/*else if (luz->getTipo() == LUZAREA) {
+			Esfera* luzArea = (Esfera*)(luz);
+			int samples = 8;
+			for (int i = 0; i < samples; i++) {
+				//float ru = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+				//float rv = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+				Vector offset = random_unit_vector();
+				Vector posicionLuz = luzArea->centro + offset * luzArea->radio;
+                //Vector posicionLuz = luzArea->posicion + (luzArea->uvec) * luzArea->llen * ru + (luzArea->lvec) * luzArea->llen * rv;
+                Vector interseccionLuz = posicionLuz - interseccion.interseccion;
+                float distanciaLuz = interseccionLuz.longitud();
+                interseccionLuz.normalizar();
+
+                float producto = interseccion.normal.escalar(interseccionLuz);
+
+                if (producto >= 0.0f) {
+                // Rayo de sombra desde el punto de interseccion a la luz
+                	Rayo rayoSombra = Rayo(interseccion.interseccion, interseccionLuz, 1, interseccion.rayo.iRefracOrigen, interseccion.rayo.rebotes);
+                	if (estaEnSombra(rayoSombra, distanciaLuz)) {
+                		// Sombra
+                		continue;
+                	}
+                	colorDifuso = (colorDifuso + (color * producto)) * luzArea->intensidad;
+                	colorEspecular = colorEspecular + calcularLuzEspecular(interseccion, luzArea);
+                }
+			}
+			colorDifuso.r = colorDifuso.r / samples;
+			colorDifuso.g = colorDifuso.g / samples;
+			colorDifuso.b = colorDifuso.b / samples;
+
+			colorEspecular.r = colorEspecular.r / samples;
+			colorEspecular.g = colorEspecular.g / samples;
+			colorEspecular.b = colorEspecular.b / samples;
+		}*/
 	}
 
 	return colorDifuso + colorEspecular;
@@ -235,7 +338,44 @@ Color calcularLuzReflexionRefraccion(const Interseccion& interseccion) {
 	return colorReflexion + colorRefraccion;
 }
 
-Vector muestreoSemiesfera(const float &r1, const float &r2) {
+//
+Color calcularLuzIndirecta(const Interseccion& interseccion, const Color& color) {
+	if (interseccion.rayo.rebotes > 1) {
+		//	return Color();
+	}
+
+	Color colorLuzIndirecta;
+	float pdf = 1 / (2 * M_PI); 
+	/*float absorbance = 1.f - max(max(color.r, color.g), color.b);
+	float random = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+	if (random > 1.f - absorbance) {
+		return Color();
+	}*/
+	//else {
+		//Vector sample = uniformSampleHemisphere(interseccion.normal);
+		//Rayo rayoSample = Rayo(interseccion.interseccion + 0.01, sample, 1, interseccion.rayo.iRefracOrigen, interseccion.rayo.rebotes + 1);
+		//colorLuzIndirecta = trazarRayo(rayoSample);
+	//}
+
+	for (int i = 0; i < LUZ_INDIRECTA_SAMPLES; i++) {
+		float r1 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		float r2 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+		Vector sample = muestreoSemiesfera(interseccion.normal, r1, r2);
+		Rayo rayoSample = Rayo(interseccion.interseccion + 0.01, sample, 1, interseccion.rayo.iRefracOrigen, interseccion.rayo.rebotes + 1);
+		colorLuzIndirecta = colorLuzIndirecta + trazarRayo(rayoSample) * r1 / pdf;
+	}
+	//cout << colorLuzIndirecta.r << " " << colorLuzIndirecta.g << " " << colorLuzIndirecta.b << endl;
+
+	colorLuzIndirecta.r = colorLuzIndirecta.r / LUZ_INDIRECTA_SAMPLES;
+	colorLuzIndirecta.g = colorLuzIndirecta.g / LUZ_INDIRECTA_SAMPLES;
+	colorLuzIndirecta.b = colorLuzIndirecta.b / LUZ_INDIRECTA_SAMPLES;
+
+	return colorLuzIndirecta;
+}
+
+Vector muestreoSemiesfera(const Vector &n, const float &r1, const float &r2) {
     float x, y, z;
 
     float phi = 2.0 * M_PI * r2;
@@ -249,29 +389,6 @@ Vector muestreoSemiesfera(const float &r1, const float &r2) {
     Vector direccion = Vector(x, y, z);
     return direccion.normalizar();
 } 
-
-//
-Color calcularLuzIndirecta(const Interseccion& interseccion, const Color& color) {
-	Color colorLuzIndirecta;
-	float pdf = 1 / (2 * M_PI); 
-
-	int samples = 2;
-	for (int i = 0; i < samples; i++) {
-		float r1 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-		float r2 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-
-		Vector sample = muestreoSemiesfera(r1, r2);
-		Rayo rayoSample = Rayo(interseccion.interseccion + 0.01, sample, 1, interseccion.rayo.iRefracOrigen, interseccion.rayo.rebotes + 1);
-		colorLuzIndirecta = colorLuzIndirecta + trazarRayo(rayoSample) * r1 / pdf;
-	}
-	//cout << colorLuzIndirecta.r << " " << colorLuzIndirecta.g << " " << colorLuzIndirecta.b << endl;
-
-	colorLuzIndirecta.r = colorLuzIndirecta.r / samples;
-	colorLuzIndirecta.g = colorLuzIndirecta.g / samples;
-	colorLuzIndirecta.b = colorLuzIndirecta.b / samples;
-
-	return colorLuzIndirecta;
-}
 
 // Vector reflectado
 Vector reflexion(const Vector& normal, const Vector& direccion) {
